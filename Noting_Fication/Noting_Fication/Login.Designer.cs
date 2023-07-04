d734b037dc4aab3fd9683a5dcb81009d6ad8646b
MinhNhatTyson <92989767+MinhNhatTyson@users.noreply.github.com>
2023-07-04 10:13:53 +0700

    Login form
    Hong ket nối database theo cách của thầy dc dùng cách này. vẫn có thể xài bth nha :)

---
+++ Noting_Fication/Noting_Fication/Login.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Noting_Fication
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            txtUsername = new TextBox();
+            txtPassword = new TextBox();
+            btnLogin = new Button();
+            label3 = new Label();
+            lblRegister = new LinkLabel();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(108, 134);
+            label1.Name = "label1";
+            label1.Size = new Size(113, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Enter password:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(108, 88);
+            label2.Name = "label2";
+            label2.Size = new Size(114, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Enter username:";
+            label2.Click += label2_Click;
+            // 
+            // txtUsername
+            // 
+            txtUsername.Location = new Point(224, 88);
+            txtUsername.Name = "txtUsername";
+            txtUsername.Size = new Size(125, 27);
+            txtUsername.TabIndex = 2;
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(224, 134);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(125, 27);
+            txtPassword.TabIndex = 3;
+            // 
+            // btnLogin
+            // 
+            btnLogin.Location = new Point(398, 88);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(94, 29);
+            btnLogin.TabIndex = 4;
+            btnLogin.Text = "Login";
+            btnLogin.UseVisualStyleBackColor = true;
+            btnLogin.Click += btnLogin_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(108, 184);
+            label3.Name = "label3";
+            label3.Size = new Size(167, 20);
+            label3.TabIndex = 5;
+            label3.Text = "Don't have an account? ";
+            // 
+            // lblRegister
+            // 
+            lblRegister.AutoSize = true;
+            lblRegister.Location = new Point(272, 184);
+            lblRegister.Name = "lblRegister";
+            lblRegister.Size = new Size(112, 20);
+            lblRegister.TabIndex = 7;
+            lblRegister.TabStop = true;
+            lblRegister.Text = "Sign up for free";
+            // 
+            // Login
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(lblRegister);
+            Controls.Add(label3);
+            Controls.Add(btnLogin);
+            Controls.Add(txtPassword);
+            Controls.Add(txtUsername);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Login";
+            Text = "Login";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox txtUsername;
+        private TextBox txtPassword;
+        private Button btnLogin;
+        private Label label3;
+        private LinkLabel lblRegister;
+    }
+}
---
+++ Noting_Fication/Noting_Fication/Login.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Noting_Fication
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+        String ConnectionString = "server =LAPTOP-P1F3R3OG\\SQLEXPRESS; database= NotingFication; uid=sa;pwd=12345; TrustServerCertificate=True";
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            var username = txtUsername.Text;
+            var pwd = txtPassword.Text;
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(ConnectionString))
+                {
+                    string query = "SELECT COUNT(*) FROM [User] WHERE Username = @Username AND Password = @Password";
+                    SqlCommand command = new SqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@Username", username);
+                    command.Parameters.AddWithValue("@Password", pwd);
+
+                    connection.Open();
+                    int userCount = (int)command.ExecuteScalar();
+                    connection.Close();
+
+                    if (userCount > 0)
+                    {
+
+                        // Successful login for an Administrator
+                        MessageBox.Show("Success", "Note", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        this.Hide();
+                        Form1 mainPage = new Form1();
+                        mainPage.Show();
+                    }
+                    else
+                    {
+                        // Failed login
+                        MessageBox.Show("Invalid username or password", "Note", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred while authenticating the user: " + ex.Message);
+            }
+        }
+    }
+}
