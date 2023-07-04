962f9c70c6d819861597a0c78b34424f46bef275
MinhNhatTyson <92989767+MinhNhatTyson@users.noreply.github.com>
2023-07-04 20:58:25 +0700

    Upload Register Form
    The form has 4 fields: Username, Password, Confirm Pass and Email. 
    In which the first 3 fields are mandatory, the Email must in correct format( abc@gmail.com)
    
    I will try to further adjust the form to look more cute :)

---
+++ Noting_Fication/Noting_Fication/Register.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace Noting_Fication
+{
+    partial class Register
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnRegister = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            txtRegisterName = new TextBox();
+            txtRegisterPassword = new TextBox();
+            txtConfirmPassword = new TextBox();
+            txtRegisterEmail = new TextBox();
+            txtRegisterPhone = new TextBox();
+            SuspendLayout();
+            // 
+            // btnRegister
+            // 
+            btnRegister.Location = new Point(429, 75);
+            btnRegister.Name = "btnRegister";
+            btnRegister.Size = new Size(94, 29);
+            btnRegister.TabIndex = 0;
+            btnRegister.Text = "Register";
+            btnRegister.UseVisualStyleBackColor = true;
+            btnRegister.Click += btnRegister_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(84, 79);
+            label1.Name = "label1";
+            label1.Size = new Size(88, 20);
+            label1.TabIndex = 1;
+            label1.Text = "Username*: ";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(84, 131);
+            label2.Name = "label2";
+            label2.Size = new Size(83, 20);
+            label2.TabIndex = 2;
+            label2.Text = "Password*: ";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(84, 191);
+            label3.Name = "label3";
+            label3.Size = new Size(138, 20);
+            label3.TabIndex = 3;
+            label3.Text = "Confirm password*:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(84, 248);
+            label4.Name = "label4";
+            label4.Size = new Size(53, 20);
+            label4.TabIndex = 4;
+            label4.Text = "Email: ";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(84, 300);
+            label5.Name = "label5";
+            label5.Size = new Size(57, 20);
+            label5.TabIndex = 5;
+            label5.Text = "Phone: ";
+            // 
+            // txtRegisterName
+            // 
+            txtRegisterName.Location = new Point(242, 76);
+            txtRegisterName.Name = "txtRegisterName";
+            txtRegisterName.Size = new Size(125, 27);
+            txtRegisterName.TabIndex = 6;
+            // 
+            // txtRegisterPassword
+            // 
+            txtRegisterPassword.Location = new Point(242, 131);
+            txtRegisterPassword.Name = "txtRegisterPassword";
+            txtRegisterPassword.Size = new Size(125, 27);
+            txtRegisterPassword.TabIndex = 7;
+            // 
+            // txtConfirmPassword
+            // 
+            txtConfirmPassword.Location = new Point(242, 191);
+            txtConfirmPassword.Name = "txtConfirmPassword";
+            txtConfirmPassword.Size = new Size(125, 27);
+            txtConfirmPassword.TabIndex = 8;
+            // 
+            // txtRegisterEmail
+            // 
+            txtRegisterEmail.Location = new Point(242, 248);
+            txtRegisterEmail.Name = "txtRegisterEmail";
+            txtRegisterEmail.Size = new Size(125, 27);
+            txtRegisterEmail.TabIndex = 9;
+            // 
+            // txtRegisterPhone
+            // 
+            txtRegisterPhone.Location = new Point(242, 300);
+            txtRegisterPhone.Name = "txtRegisterPhone";
+            txtRegisterPhone.Size = new Size(125, 27);
+            txtRegisterPhone.TabIndex = 10;
+            // 
+            // Register
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(txtRegisterPhone);
+            Controls.Add(txtRegisterEmail);
+            Controls.Add(txtConfirmPassword);
+            Controls.Add(txtRegisterPassword);
+            Controls.Add(txtRegisterName);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(btnRegister);
+            Name = "Register";
+            Text = "Register";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnRegister;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private TextBox txtRegisterName;
+        private TextBox txtRegisterPassword;
+        private TextBox txtConfirmPassword;
+        private TextBox txtRegisterEmail;
+        private TextBox txtRegisterPhone;
+    }
+}
---
+++ Noting_Fication/Noting_Fication/Register.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Noting_Fication
+{
+    public partial class Register : Form
+    {
+        public Register()
+        {
+            InitializeComponent();
+        }
+        String ConnectionString = "server =LAPTOP-P1F3R3OG\\SQLEXPRESS; database= NotingFication; uid=sa;pwd=12345; TrustServerCertificate=True";
+
+        // This function was born because I eam to lazy to add the auto incrementing into the User table
+        private int GenerateNewUserID()
+        {
+            int newUserID = 1; // Start with a default value
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(ConnectionString))
+                {
+                    string query = "SELECT MAX(UserID) FROM [User]";
+                    SqlCommand command = new SqlCommand(query, connection);
+
+                    connection.Open();
+                    var result = command.ExecuteScalar();
+                    connection.Close();
+
+                    if (result != null && result != DBNull.Value)
+                    {
+                        newUserID = Convert.ToInt32(result) + 1;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred while generating a new UserID: " + ex.Message);
+            }
+
+            return newUserID;
+        }
+
+        // Function to valid the correct email format
+        private bool IsValidEmail(string email)
+        {
+            // Regular expression pattern for email validation
+            string emailPattern = @"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$";
+
+            // Use Regex.IsMatch to validate the email format
+            return Regex.IsMatch(email, emailPattern);
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            string username = txtRegisterName.Text;
+            string password = txtRegisterPassword.Text;
+            string confirmPassword = txtConfirmPassword.Text;
+            string email = txtRegisterEmail.Text;
+
+            // Check if user has entered all the field with the *
+            if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password) || string.IsNullOrWhiteSpace(confirmPassword))
+            {
+                MessageBox.Show("Please fill in all required fields.", "Registration", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // Check if password and confirm password match
+            if (password != confirmPassword)
+            {
+                MessageBox.Show("Password and Confirm Password do not match.", "Registration", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // Check if the email is in correct format
+            if (!IsValidEmail(email))
+            {
+                MessageBox.Show("Please enter a valid email address( ex: abc@gmail.com).", "Registration", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(ConnectionString))
+                {
+                    string query = "INSERT INTO [User](UserID, UserName, Password, Email) VALUES (@UserID, @Username, @Password, @Email)";
+                    SqlCommand command = new SqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@UserID", GenerateNewUserID());
+                    command.Parameters.AddWithValue("@Username", username);
+                    command.Parameters.AddWithValue("@Password", password);
+                    command.Parameters.AddWithValue("@Email", email);
+
+                    connection.Open();
+                    int rowsAffected = command.ExecuteNonQuery();
+                    connection.Close();
+
+                    if (rowsAffected > 0)
+                    {
+                        MessageBox.Show("Registration successful.", "Registration", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        this.Close();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Registration failed. Please try again.", "Registration", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred during registration: " + ex.Message, "Registration", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
